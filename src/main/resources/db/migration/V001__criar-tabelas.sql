a30c1d33ef56e5a438293c3bf0191f3be3898c42
Gustavo Almeida <gualmeida2004@gmail.com>
2023-10-21 08:07:49 -0300

    feat: Configuração do ambiente

---
+++ src/main/resources/db/migration/V001__criar-tabelas.sql
@@ -0,0 +1,50 @@
+CREATE TABLE gestor(
+	gestor_id uuid PRIMARY KEY NOT NULL,
+	nome TEXT NOT NULL,
+	email TEXT NOT NULL,
+	senha TEXT NOT NULL
+);
+
+CREATE TABLE categoria(
+    categoria_id serial PRIMARY KEY,
+    nome_categoria TEXT NOT NULL,
+    porcentagem_limite numeric(3, 2) NOT NULL
+);
+
+CREATE TABLE despesa(
+    despesa_id serial PRIMARY KEY,
+    nome_despesa TEXT NOT NULL,
+    definir_lembrete BOOLEAN NOT NULL,
+    gestor_id uuid NOT NULL,
+    categoria_id INTEGER NOT NULL,
+
+	CONSTRAINT fk_gestor_id
+	FOREIGN KEY(gestor_id) REFERENCES gestor(gestor_id)
+	ON DELETE CASCADE,
+
+	CONSTRAINT fk_categoria_id
+	FOREIGN KEY(categoria_id) REFERENCES categoria(categoria_id)
+	ON DELETE NO ACTION
+);
+
+CREATE TABLE recorrencia_despesa(
+	recorrencia_id serial PRIMARY KEY,
+	frequencia TEXT NOT NULL,
+	quantidade INTEGER NOT NULL,
+	despesa_id INTEGER NOT NULL,
+
+	CONSTRAINT fk_despesa_id
+	FOREIGN KEY(despesa_id) REFERENCES despesa(despesa_id)
+	ON DELETE CASCADE
+);
+
+CREATE TABLE registro_despesa(
+	registro_despesa_id serial PRIMARY KEY,
+	valor NUMERIC NOT NULL,
+	data DATE NOT NULL,
+	despesa_id INTEGER NOT NULL,
+
+	CONSTRAINT fk_despesa_id
+	FOREIGN KEY(despesa_id) REFERENCES despesa(despesa_id)
+	ON DELETE CASCADE
+);
